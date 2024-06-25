89ec22885a344353c74d77c49486e3242121f296
Nakul <nakulsingh9588@gmail.com>
2024-06-25 17:10:39 +0530

    post files added

---
+++ post.js
@@ -0,0 +1,7 @@
+// You can add any interactive JavaScript functionality here, if needed.
+// For example, if you want to dynamically add photo sections, you could write
+// functions to manipulate the DOM and insert new elements.
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Add JavaScript functionality if needed
+});
